822a60eb703af9b9b4157d0bb5e7f23e370a312d
luncang1988@163.com <luncang1988@163.com>
2020-08-15 16:48:55 +0800

    使用自定义字体

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>home</span>
+    <span class="icon-bookmark"></span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home'
+
   }
-}
+
 </script>
+<style lang="less" scoped>
+ .icon-bookmark{
+   color: red;
+   font-size: 100px;
+ }
+
+</style>
